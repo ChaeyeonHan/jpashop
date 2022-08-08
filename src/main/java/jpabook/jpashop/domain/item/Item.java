4d91ee0fd00f4997be5312255f2cfb576d0001a4
ChaeyeonHan <kate2236e@naver.com>
2022-08-09 02:29:59 +0900

    [FEAT] item

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직 ==//
+    // stockQuantity 데이터를 가지고 있는 쪽에 비즈니스 로직을 작성해준다
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소(0보다 작아지지 않도록 체크)
+     */
+    public void removeStock(int quantity){
+        int resStock = this.stockQuantity - quantity;
+        if (resStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+    }
 }
