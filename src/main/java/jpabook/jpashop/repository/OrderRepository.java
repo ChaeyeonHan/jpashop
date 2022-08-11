dc630a1f555ed4bac2bd57a1efc664ae489f9f7a
ChaeyeonHan <kate2236e@naver.com>
2022-08-11 13:39:28 +0900

    [FEAT] find orderItem

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,7 +21,18 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){
-//
-//    }
+    // 동적쿼리
+
+    /**
+     * JPA Criteria
+     */
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)  // 최대 1000개까지 조회된다 -> 페이징 처리 가능
+                .getResultList();
+    }
 }
